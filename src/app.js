d4d69da6fd5d9263ac2eebea497d91a443f9703f
Lucas.Fernandes <lucas.fernandes001p@gmail.com>
2019-01-10 10:51:58 -0200

    Organizing

--- src/app.js
+++ src/app.js
@@ -1,7 +1,27 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import AppRouter from './routers/AppRouter';
+import configStore from './store/configureStore';
+import { addExpense } from './actions/expenses';
+import { setTextFilter } from './actions/filters';
+import getVisibleExpenses from './selectors/expenses';
 import 'normalize.css/normalize.css';
 import './styles/styles.scss';
+
+
+
+const store = configStore();
+
+store.dispatch(addExpense({description: 'Water bill'}));
+store.dispatch(addExpense({description: 'Gas bill'}));
+store.dispatch(setTextFilter('bill'));
+
+const state = store.getState();
+const VisibleExpenses = getVisibleExpenses(state.expenses, state.filters);
+console.log(visibleExpenses);
+
+
+
+console.log(store.getState());
 
 ReactDOM.render(<AppRouter />, document.getElementById('app'));
