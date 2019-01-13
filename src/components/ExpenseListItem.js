59c0f61bfa851be6980706a6317df6e447f1cf08
Lucas.Fernandes <lucas.fernandes001p@gmail.com>
2019-01-13 07:41:45 -0200

    INPUT WITH FILTERS

--- src/components/ExpenseListItem.js
+++ src/components/ExpenseListItem.js
@@ -1,10 +1,19 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import { removeExpense } from '../actions/expenses';
 
-const ExpenseListItem = ({ description, amount, createdAt }) => (
+const ExpenseListItem = ({ dispatch, description, id, amount, createdAt }) => (
   <div>
     <h3>{description}</h3>
     <p>{amount} - {createdAt}</p>
+    {    console.log(id) }
+    <input type="button" Value="Remove" onClick={() => {
+            dispatch(removeExpense({ id }))
+            console.log(id);
+        }}/>
   </div>
 );
 
-export default ExpenseListItem;
+
+
+export default connect()(ExpenseListItem);
