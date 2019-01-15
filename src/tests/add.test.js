9be202ed0caa2d930c59e8b937447512cbf1c077
Lucas.Fernandes <lucas.fernandes001p@gmail.com>
2019-01-15 04:00:09 -0200

    adding jest on app

---
+++ src/tests/add.test.js
@@ -0,0 +1,20 @@
+const add = (a, b) => a + b;
+const generateGreeting = (name = 'Anonymous') => `Hello ${name}!`;
+
+
+test('should add two numbers', () => {
+    const result = add(3, 4);
+    expect(result).toBe(7);
+});
+
+
+test('should generate greeting from name', () => {
+    const result = generateGreeting("Lucas");
+    expect(result).toBe(`Hello Lucas!`);
+});
+
+test('should generate greeting for no name', () => {
+    const result = generateGreeting();
+    expect(result).toBe(`Hello Anonymous!`);
+});
+
