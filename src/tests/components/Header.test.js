5928609e59deb0f59c73e92f9ccc8fdffcce77d2
Lucas.Fernandes <lucas.fernandes001p@gmail.com>
2019-01-16 15:49:06 -0200

    Snapshot testing with dynamic Components

--- src/tests/components/Header.test.js
+++ src/tests/components/Header.test.js
@@ -1,13 +1,17 @@
 // react-test-renderer
 import React from 'react';
-import ShallowRenderer from 'react-test-renderer/shallow';
+import { shallow } from 'enzyme';
 import Header from '../../components/Header'
 
 
 test('should render Header correctly', () => {
-    const renderer = new ShallowRenderer();
-    renderer.render(<Header />);
-    expect(renderer.getRenderOutput()).toMatchSnapshot();
+    const wrapper = shallow(<Header />);
+    expect(wrapper).toMatchSnapshot();
+    
+    // expect(wrapper.find('h1').text()).toBe("Expensify");
+    // const renderer = new ShallowRenderer();
+    // renderer.render(<Header />);
+    // expect(renderer.getRenderOutput()).toMatchSnapshot();    
 });
 
 
